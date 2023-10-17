dc34218441fc81aeb8913d24ebb931f9ba5ceb17
Admin <Admin@DESKTOP-5E61P89>
2023-10-17 14:29:25 +0700

    Commit code counter function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { counterReducer } from './shared/store/counter.reducer';
+import { StoreModule } from '@ngrx/store';
+import { CounterdisplayComponent } from './component/counterdisplay/counterdisplay.component';
+import { CounterbuttonComponent } from './component/counterbutton/counterbutton.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './Material.Module';
+import { CustomcounterComponent } from './component/customcounter/customcounter.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterdisplayComponent,
+    CounterbuttonComponent,
+    CustomcounterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    StoreModule.forRoot({ counter: counterReducer }),
+    BrowserAnimationsModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
